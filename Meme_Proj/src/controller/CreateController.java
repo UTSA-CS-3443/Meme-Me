a1e5f4b1f0ff2c37617fbcdf0b8d9250600126c8
Cheyenne Sanchez <cza267@my.utsa.edu>
2017-11-25 23:08:30 -0600

    //More Updates- bkgd is black, updates on undo redo

--- Meme_Proj/src/controller/CreateController.java
+++ Meme_Proj/src/controller/CreateController.java
@@ -1,22 +1,24 @@
 package controller;
 
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -27,6 +29,8 @@ import javafx.stage.FileChooser.ExtensionFilter;
 
 public class CreateController implements EventHandler<ActionEvent>{
 	
+	@FXML
+	public Canvas canvas;
 	@FXML
 	private StackPane pane;
 	//TODO: ToolPane EDIT
@@ -47,18 +51,14 @@ public class CreateController implements EventHandler<ActionEvent>{
 	 @FXML
 	 private TextField txtFd;
 	//TODO: FILE 
-	public Image original;
-	
-	public Image saved;
 	
 	public ImageView imgView;
+	
 
 	
 	@Override
-	public void handle(ActionEvent event) {
-		//For implements leave alone
-		}
-	
+	public void handle(ActionEvent event) {		}
+		
 	//File Menu	
 	@FXML
 	public void openFunction(ActionEvent event) throws IOException {		
@@ -72,6 +72,7 @@ public class CreateController implements EventHandler<ActionEvent>{
 			Image img = new Image(location);
 			System.out.println("height: " +img.getHeight() + "\nWidth: " + img.getWidth());
 			imgView.setImage(img);
+			pane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 		}
 	}	
 	
@@ -98,32 +99,38 @@ public class CreateController implements EventHandler<ActionEvent>{
 	}
 
 	//Edit Menu	
-	public void undoFunction(ActionEvent event) {	}
-	
-	public void redoFunction(ActionEvent event){	}
+	@SuppressWarnings("rawtypes")
+	@FXML
+	public void undoFunction(ActionEvent event) {
+		UndoRedoStack stack = new UndoRedoStack();
+		stack.undo();
+		
+	}
+	@SuppressWarnings("rawtypes")
+	@FXML
+	public void redoFunction(ActionEvent event){
+		UndoRedoStack stack = new UndoRedoStack();
+		stack.redo();
+	}
 	
 	public void clearFunction(ActionEvent event) {
-		pane.getChildren().clear(); 
-		System.out.print("Cleared All");		
+		pane.getChildren().clear(); 	
 	} 
 	
-	//Tools			
-		
+	//Tools				
 	@FXML
 	public void topLine(ActionEvent event) {
 		try {
-	
 		TextField txtFd  = new TextField();
 		Text text = new Text();
-		txtFd.setText("Default Text");
-		
+		txtFd.setText("Default Text");		
 		text.textProperty().bind(txtFd.textProperty());
 		text.getText();
-		text.setFont(Font.font("Impact",FontWeight.BOLD,70));
+		text.setFont(Font.font("Impact",FontWeight.BOLD,50));
 		text.setFill(Color.WHITE);
 		text.setStroke(Color.BLACK);
-		text.setX(50);
-		text.setY(50);
+		text.setX(30);
+		text.setY(70);
 		group.getChildren().add(text);
 		}
 		catch(Exception ex) {
